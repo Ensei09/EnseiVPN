6179e3bdbdaf8f897249c8a9c78a125477ca4f45
Ensei09 <70925165+Ensei09@users.noreply.github.com>
2023-02-02 09:46:15 +0800

    removed autoreboot

--- enseivpn_vpsinstaller.sh
+++ enseivpn_vpsinstaller.sh
@@ -487,8 +487,8 @@ EOM
 
 crontab -r
 (crontab -l 2>/dev/null || true; echo "*/5 * * * * /bin/bash /root/cron.sh") | crontab -
-(crontab -l 2>/dev/null || true; echo "0 4 * * * /sbin/shutdown -r") | crontab -
-(crontab -l 2>/dev/null || true; echo "0 0 * * * /sbin/shutdown -r") | crontab -
+#(crontab -l 2>/dev/null || true; echo "0 4 * * * /sbin/shutdown -r") | crontab -
+#(crontab -l 2>/dev/null || true; echo "0 0 * * * /sbin/shutdown -r") | crontab -
 #printf "\nAllowUsers root" >> /etc/ssh/sshd_config
 chmod -R 755 /etc/openvpn
 apt remove apache2 -y
