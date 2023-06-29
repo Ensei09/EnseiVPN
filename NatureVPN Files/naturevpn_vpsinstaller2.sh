0bcb57ac2cb043676bf36c0f8c1dffe3a02bae58
Ensei09 <70925165+Ensei09@users.noreply.github.com>
2023-06-29 20:10:33 +0800

    Update naturevpn_vpsinstaller2.sh

--- NatureVPN Files/naturevpn_vpsinstaller2.sh
+++ NatureVPN Files/naturevpn_vpsinstaller2.sh
@@ -360,6 +360,7 @@ confdir /etc/privoxy
 logdir /var/log/privoxy
 filterfile default.filter
 logfile logfile
+listen-address  0.0.0.0:8118
 listen-address  0.0.0.0:8000
 listen-address  0.0.0.0:8080
 listen-address  0.0.0.0:8888
